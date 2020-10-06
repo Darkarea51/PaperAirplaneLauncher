05a23f0cf444f9d9b30c2c3b2fa2fe0d4e4e4b64
Carson <46657662+Darkarea51@users.noreply.github.com>
2020-10-06 15:46:05 -0700

    Create .cc

---
+++ .cc
@@ -0,0 +1,26 @@
+/* Outline */ 
+
+**Pin inputs 
+
+/*
+void setup() { 
+
+lcd1 set up
+lcd2 setup
+pin motor 
+knob setup 1
+knob setup 2
+
+} 
+
+void loop() {
+
+lcd turn on display
+analogwrite(motorpin,speed)
+knob setup 1 increase and decrease 
+knob setup 2 increase and decrease
+
+send knob number1 to lcd1
+send knob number2 to lcd2
+
+} 
